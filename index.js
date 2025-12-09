9881e6dae0d3fd2e4e165d66da74c2fab76efef6
vcarbonell <vcarbonell@plasfesa.com>
2025-12-09 11:41:55 +0100

    aumenta límite de body para alta previsiones

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ require('dotenv').config();  // Importante para leer el .env
 const { poolPromise } = require("./db");
 
 const app = express();
+// Aumenta el límite, por ejemplo a 10 MB
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+
 app.use(express.json());
 
 // Middleware para validar api_key
