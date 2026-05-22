7e6a82ff88f3d8d16d8a9aee82c935940abbc6bd
vcarbonell <vcarbonell@plasfesa.com>
2026-05-22 14:04:20 +0200

    error fecha fin

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ app.post("/addForecast", async (req, res) => {
       request.input("concepto", sql.VarChar, (concepto === null) ? 'Base' : concepto);
       request.input("FechaFin", sql.Int, FechaFin);
 
-      // console.log("concepto:", concepto);
+      console.log("FechaFin:", FechaFin);
 
 
       const query = `
